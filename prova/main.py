5927e4d90207ad761df965bb40680e4632af015b
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2023-03-31 16:01:37 -0300

    ...

--- prova/main.py
+++ prova/main.py
@@ -1,9 +1,13 @@
 import add
 from jogo import Jogo
-
+from sqlalchemy import select
 
 add.run()
 
 print(add.session.query(Jogo).all())
 
 
+stmt = select(Jogo)
+
+for user in add.session.scalars(stmt):
+    print(user)
